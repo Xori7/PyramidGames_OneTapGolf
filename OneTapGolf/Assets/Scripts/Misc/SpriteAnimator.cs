a3fe14cfec382f672908fd607ea56c91eee50afe
Piotrku8192 <pszulakowski321@gmail.com>
2021-06-15 21:46:47 +0200

    Add map and target prefab

---
+++ OneTapGolf/Assets/Scripts/Misc/SpriteAnimator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteAnimator : MonoBehaviour {
+    private SpriteRenderer spriteRenderer;
+
+    public Sprite[] sprites;
+    public float frameDuration;
+
+    private int currentSpriteId;
+
+    private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        StartCoroutine(UpdateSprite());
+    }
+
+    private IEnumerator UpdateSprite() {
+        while (true) {
+            yield return new WaitForSeconds(frameDuration);
+            ChangeSprite();
+        }
+    }
+
+    private void ChangeSprite() {
+        if (sprites == null || sprites.Length == 0) {
+            throw new System.Exception("No sprites have been assigned to the animator!");
+        }
+
+        spriteRenderer.sprite = sprites[currentSpriteId];
+        
+        currentSpriteId++;
+        if (currentSpriteId == sprites.Length) {
+            currentSpriteId = 0;
+        }
+    }
+}
