0016c4d98f12b395a486563b34723642c1f648d6
Piotrku8192 <pszulakowski321@gmail.com>
2021-06-18 20:31:24 +0200

    Fix controlling the ball by mouse button

--- OneTapGolf/Assets/Scripts/Ball/BallObject.cs
+++ OneTapGolf/Assets/Scripts/Ball/BallObject.cs
@@ -4,22 +4,28 @@ using System.Linq;
 using UnityEngine;
 
 namespace OneTapGolf.Ball {
+    public enum BallState {
+        Static,
+        Wait,
+        Throwed
+    }
+
     public class BallObject : MonoBehaviour {
         public GameObject routePointPrefab;
         public BallController BallController { get; private set; }
+        public BallState state;
+        public float xVelocity, yVelocity;
         private List<GameObject> routePoints = new List<GameObject>();
         private BallRoutePointsGenerator routePointsGenerator;
         private float timeElapsed;
 
-        public float xVelocity, yVelocity;
-        private bool throwed;
 
         public void Initialize() {
             StopAllCoroutines();
             BallController = new BallController(transform.position, Physics2D.gravity, -1.68f, -0.1f, 0.4f);
             routePointsGenerator = new BallRoutePointsGenerator(BallController);
             timeElapsed = 3;
-            throwed = false;
+            state = BallState.Static;
         }
 
         private void Start() {
@@ -27,21 +33,27 @@ namespace OneTapGolf.Ball {
         }
 
         private void FixedUpdate() {
-            if (Input.GetButtonDown("Fire1")) {
-                Throw();
-            }
-
-            if (throwed) {
-                BallController.OnUpdate(Time.fixedDeltaTime);
-                transform.position = BallController.physicalObject.position;
-            }
-            else {
-                WhileWaiting();
+            switch (state) {
+                case BallState.Static:
+                    if (Input.GetButtonDown("Fire1")) {
+                        state = BallState.Wait;
+                    }
+                    break;
+                case BallState.Throwed:
+                    BallController.OnUpdate(Time.fixedDeltaTime);
+                    transform.position = BallController.physicalObject.position;
+                    break;
+                case BallState.Wait:
+                    WhileWaiting();
+                    if (Input.GetButtonUp("Fire1")) {
+                        Throw();
+                        state = BallState.Throwed;
+                    }
+                    break;
             }
         }
 
         private void Throw() {
-            throwed = true;
             BallController.Throw(new Vector2(xVelocity, yVelocity) * timeElapsed);
             foreach (GameObject point in routePoints) {
                 point.gameObject.SetActive(false);
@@ -51,7 +63,7 @@ namespace OneTapGolf.Ball {
 
         private IEnumerator Wait() {
             yield return new WaitForSeconds(5);
-            if (throwed) {
+            if (state == BallState.Throwed) {
                 GameManager.Singleton.Lost();
             }
         }
