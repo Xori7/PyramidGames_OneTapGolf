db2d2357f71791657438035b9bdf13125c75d725
Piotrku8192 <pszulakowski321@gmail.com>
2021-06-18 12:27:01 +0200

    Add bounce when touching ground and ground friction

--- OneTapGolf/Assets/Scripts/Ball/Ball.cs
+++ OneTapGolf/Assets/Scripts/Ball/Ball.cs
@@ -7,13 +7,20 @@ namespace OneTapGolf.Ball {
         private readonly BallObject ballObject;
         private readonly IPhysicalObjectVelocityUpdater velocityUpdater;
         private readonly IPhysicalObjectPositionUpdater positionUpdater;
+        private readonly float groundLevel;
+        private readonly float groundFrictionAcceleration;
+        private readonly float bounceAccelerationLoss;
 
-        public Ball(BallObject ballObject, Vector2 position, Vector2 gravityForce) {
-            this.ballObject = ballObject;
+        public Ball(BallObject ballObject, Vector2 position, Vector2 gravityAcceleration, 
+            float groundLevel, float groundFrictionAcceleration, float bounceAccelerationLoss) {
             physicalObject = new PhysicalObject(position);
-            physicalObject.AddForce(gravityForce);
+            physicalObject.AddAcceleration(gravityAcceleration);
+            this.ballObject = ballObject;
             velocityUpdater = new DefaultPhysicalObjectVelocityUpdater(physicalObject);
             positionUpdater = new DefaultPhysicalObjectPositionUpdater(physicalObject);
+            this.groundLevel = groundLevel;
+            this.groundFrictionAcceleration = groundFrictionAcceleration;
+            this.bounceAccelerationLoss = bounceAccelerationLoss;
         }
 
         public void Throw(Vector2 startVelocity) {
@@ -21,9 +28,22 @@ namespace OneTapGolf.Ball {
         }
 
         public void OnUpdate(float timeElapsed) {
+            if (physicalObject.position.y < groundLevel) {
+                OnCollisionWithGround();
+                //Calculate ground friction
+                physicalObject.velocity += new Vector2(Mathf.Clamp(groundFrictionAcceleration, -physicalObject.velocity.x, float.MaxValue), 0);
+            }
             velocityUpdater.UpdatePhysicalObjectVelocity(timeElapsed);
             positionUpdater.UpdatePhysicalObjectPosition(timeElapsed);
+
             ballObject.transform.position = physicalObject.position;
+        }
+        private void OnCollisionWithGround() {
+            //Set y position to ground position when ball tries to escape the map
+            physicalObject.position = new Vector2(physicalObject.position.x, groundLevel);
+
+            //Add velocity in opposite direction as in 3 Newton's law of motion
+            Throw(new Vector2(0, -physicalObject.velocity.y * (2 - bounceAccelerationLoss)));
         }
     }
 }
--- OneTapGolf/Assets/Scripts/Ball/BallObject.cs
+++ OneTapGolf/Assets/Scripts/Ball/BallObject.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 namespace OneTapGolf.Ball {
     public class BallObject : MonoBehaviour {
         private Ball ball;
+
         private void Start() {
-            ball = new Ball(this, new Vector2(), new Vector2(0, -9.8f));
-            ball.Throw(new Vector2(4f, 12));
+            ball = new Ball(this, new Vector2(-9, 0f), new Vector2(0, -9.8f), -1.68f, -0.01f, 0.4f);
+            ball.Throw(new Vector2(1f, 12));
         }
 
-        private void Update() {
-            ball.OnUpdate(Time.deltaTime);
+        private void FixedUpdate() {
+            ball.OnUpdate(Time.fixedDeltaTime);
         }
     }
 }
